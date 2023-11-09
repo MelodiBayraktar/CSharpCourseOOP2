6116ff008366fbd77e9a18152b43be667ed66596
MelodiBayraktar <melodi_bayraktar@hotmail.com>
2023-11-09 19:19:20 +0300

    Inheritance ile çalışmalar yapıldı

---
+++ OPP1_2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace OPP1_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Musteri musteri1 = new Musteri();
+        musteri1.Ad = " Engin";
+        musteri1.Soyad = "Demiroğ";
+        musteri1.Id = 1;
+        musteri1.TcNo = "222222222";
+        musteri1.MusteriNo = "12345";
+        musteri1.SirketAdi = "?";
+        //gerçek müşteri - tüzel müşteri
+
+        //SOLID 
+
+
+        Console.ReadLine();
+    }
+}
+
