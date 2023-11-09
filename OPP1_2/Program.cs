caaa01323a138bcecf388aad736c3e1477e9114c
MelodiBayraktar <melodi_bayraktar@hotmail.com>
2023-11-09 19:19:53 +0300

    Inheritance ile yapılan çalışmalar güncellendi

--- OPP1_2/Program.cs
+++ OPP1_2/Program.cs
@@ -4,17 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Musteri musteri1 = new Musteri();
-        musteri1.Ad = " Engin";
-        musteri1.Soyad = "Demiroğ";
+        GercekMusteri musteri1 = new GercekMusteri();
         musteri1.Id = 1;
-        musteri1.TcNo = "222222222";
         musteri1.MusteriNo = "12345";
-        musteri1.SirketAdi = "?";
+        musteri1.Ad = "Engin";
+        musteri1.Soyad = "Demiroğ";
+        musteri1.TcNo = "123456789";
+        //kodlama io
+        TuzelMusteri musteri2 = new TuzelMusteri();
+        musteri2.Id = 2;
+        musteri2.MusteriNo = "142323";
+        musteri2.SirketAdi = "Kodlama.io";
+        musteri2.VergiNo = "32434434";
+
+
         //gerçek müşteri - tüzel müşteri
 
         //SOLID 
 
+        Musteri musteri3 = new GercekMusteri();
+        Musteri musteri4 = new TuzelMusteri();
+
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add(musteri1);
+        customerManager.Add(musteri2);
 
         Console.ReadLine();
     }
