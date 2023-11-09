2a68e1d85b5f7c4c0b358b3a102c0dc9d820cb26
MelodiBayraktar <melodi_bayraktar@hotmail.com>
2023-11-09 22:03:45 +0300

    Interface ile çalışmalar yapıldı

---
+++ OOP1_3/BasvuruManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP1_3
+{
+	public class BasvuruManager
+	{
+		public void BasvuruYap(IKrediManager krediManager)
+		{
+			//Başvuran bilgilerini değerlendirme
+			krediManager.Hesapla();
+		}
+
+		public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+		{
+			foreach (var kredi in krediler)
+			{
+				kredi.Hesapla();
+			}
+		}
+	}
+}
+
---
+++ OOP1_3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace OOP1_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+        //ihtiyacKrediManager.Hesapla();
+
+        //TasitKrediManager tasitKrediManager = new TasitKrediManager();
+        //tasitKrediManager.Hesapla();
+
+        //KonutKrediManager konutKrediManager = new KonutKrediManager();
+        //konutKrediManager.Hesapla();
+
+        IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+
+        IKrediManager tasitKrediManager = new TasitKrediManager();
+      
+        IKrediManager konutKrediManager = new KonutKrediManager();
+
+        BasvuruManager basvuruManager = new BasvuruManager();
+        //basvuruManager.BasvuruYap(ihtiyacKrediManager);
+
+        List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager };
+        basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        Console.ReadLine();
+    }
+}
+
